b8e2f7ce2580b7987c65ae7c2736ad1e2fa56c29
hanraeul <toast17@naver.com>
2024-05-17 11:44:02 +0900

    init commit

---
+++ src/test/java/com/sparta/springreport/SpringReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
